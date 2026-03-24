9d23713c6b3e76300f66e5f724b5a83c9ecdca97
OskarroGame <oskarro13@protonmail.com>
2026-03-24 21:51:53 +0100

    Added Viruses

--- main.lua
+++ main.lua
@@ -1,16 +1,39 @@
 function love.load()
+    math.randomseed(os.time())
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.window.setTitle("Terminator")
-
     player = {
         x = love.graphics.getWidth() / 2,
         y = love.graphics.getHeight() / 2,
         spd = 200,
         sprite = love.graphics.newImage("Gracz.png")
     }
+    viruses = {}
+
+    wirus = {
+        x = math.random(0, love.graphics.getWidth() - 100),
+        y = math.random(0, love.graphics.getHeight() - 50),
+        type = "green"
+    }
+    table.insert(viruses, wirus)
+
+    cpuUsage = 0
+    timer = 0
 end
 
 function love.update(dt)
+    timer = timer + dt
+    if timer >= 2 then
+        nowy = {
+            x = math.random(0, love.graphics.getWidth() - 100),
+            y = math.random(0, love.graphics.getHeight() - 50),
+            type =
+            "green"
+        }
+        table.insert(viruses, nowy)
+        timer = 0
+    end
+
     if love.keyboard.isDown("d") then move_right(dt) end
     if love.keyboard.isDown("a") then move_left(dt) end
 end
@@ -32,4 +55,20 @@ function love.draw()
 
     -- Gracz
     love.graphics.draw(player.sprite, player.x, player.y, 0, 3, 3, ox, oy)
+
+    -- Wirusy
+    for i, v in ipairs(viruses) do
+        if v.type == "green" then
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.circle("fill", v.x, v.y, 15, 15)
+        end
+    end
+
+    -- Ramka
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("line", 10, 10, 200, 30)
+
+    -- Pasek
+    love.graphics.setColor(1, 0.8, 0)
+    love.graphics.rectangle("fill", 10, 10, cpuUsage * 2, 30)
 end
